08954d6f15d605cca59ee90680fe1f4646c5fad5
Neto test <bolsvet190679@mail.ru>
2023-08-24 11:25:41 +0300

    test sucessfull

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnDataSent() {
+               given()
+                .baseUri("https://postman-echo.com")
+                .body("Make Love,Not War")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo ("Make Love,Not War"))
+        ;
+    }
+}
